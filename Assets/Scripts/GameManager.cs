f3274544a156e4575e826163d5aeff1810aa2fa0
GhettoNoodles <jcoetzee2201@gmail.com>
2023-11-27 15:47:57 +0200

    Final stuff before testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,9 @@ namespace Com.SpeakGeek.JacquesPun
         #region Public Fields
 
         public static GameManager Instance;
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
 
         #endregion
         
@@ -54,6 +57,16 @@ namespace Com.SpeakGeek.JacquesPun
         private void Start()
         {
             Instance = this;
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
+            }
         }
 
         #endregion
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -22,8 +22,27 @@ namespace Com.SpeakGeek.JacquesPun
 
         #endregion
 
+        #region Public Fields
+
         [Tooltip("The current Health of our player")]
         public float Health = 1f;
+
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        #endregion
+
+        #region Private Methods
+
+#if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene,
+            UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+        {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+#endif
+
+        #endregion
 
         #region MonoBehaviour CallBacks
 
@@ -40,6 +59,15 @@ namespace Com.SpeakGeek.JacquesPun
             {
                 beams.SetActive(false);
             }
+
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+
+// #Critical
+// we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
         }
 
         void Start()
@@ -56,6 +84,26 @@ namespace Com.SpeakGeek.JacquesPun
             else
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            }
+#if UNITY_5_4_OR_NEWER
+// Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+#endif
+        }
+#if !UNITY_5_4_OR_NEWER
+/// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+void OnLevelWasLoaded(int level)
+{
+    this.CalledOnLevelWasLoaded(level);
+}
+#endif
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
             }
         }
 
@@ -149,6 +197,14 @@ namespace Com.SpeakGeek.JacquesPun
 
         #endregion
 
+#if UNITY_5_4_OR_NEWER
+        public override void OnDisable()
+        {
+            // Always call the base to remove callbacks
+            base.OnDisable();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+#endif
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
             if (stream.IsWriting)
